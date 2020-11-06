1128dc34a2de89508a35fb880f8be6ced0d759ef
Daniel Torres <danielandrestv@hotmail.com>
2020-11-05 19:02:51 -0500

    first commit

---
+++ src/app/shared/plans/data.ts
@@ -0,0 +1,25 @@
+const pricingData = [
+    {
+        title: 'Economy',
+        price: 19,
+        bandwidth: 'Procesos',
+        onlinespace: '50MB',
+        support: 'No'
+    },
+    {
+        title: 'Premium',
+        price: 29,
+        bandwidth: 'Camara de frío',
+        onlinespace: '1GB',
+        support: 'No'
+    },
+    {
+        title: 'Developer',
+        price: 39,
+        bandwidth: 'Patio de maniobras',
+        onlinespace: '2GB',
+        support: 'Yes'
+    },
+];
+
+export { pricingData };
---
+++ src/app/shared/services/data.ts
@@ -0,0 +1,10 @@
+const serviceData = [
+    { icon: 'grid', title: 'Revestimientos para pisos industriales', text: 'To an English person, it will seem like English as skeptical.' },
+    { icon: 'edit', title: 'Impermiabilización de techos, terrazas y cisternas', text: 'If several languages coalesce, the grammar of the language.' },
+    { icon: 'headphones', title: 'Impermiabilización para pisos en estacionamientos', text: 'The languages only differ in their grammar their pronunciation' },
+    /* { icon: 'layers', title: 'Creative Design', text: 'Everyone realizes why a new common would be desirable.' },
+    { icon: 'code', title: 'Quality Code', text: 'To achieve this, it would be necessary to have uniform.' },
+    { icon: 'tablet', title: 'Responsive layout', text: 'Their separate existence is a myth. For science, music, etc.' }, */
+];
+
+export { serviceData };
